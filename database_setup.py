42ada2aa801eed2f2cf74037dae8349b721b683d
Matthew Yasul <yasulmatthew@gmail.com>
2019-03-03 10:24:21 +0800

    Add SQLAchemy and SQLite setup

---
+++ database_setup.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, func, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class Dictionary(Base):
+    __tablename__ = 'dictionary'
+
+    kapampangan = Column(String(250), primary=True)
+    tagalog = Column(String(250), primary=True)
+    english = Column(String(250), nullable=False)
+    date_created = Column(DateTime(timezone=True), server_default=func.now())
+
+
+engine = create_engine('sqlite:///ekt_dictionary.db')
+
+Base.metadata.create_all(engine)
--- main.py
+++ main.py
@@ -1,12 +1,46 @@
+# Imports from SQLite library
+from sqlalchemy import create_engine, asc, func, select, exc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Dictionary, Base
+
+# Imports from Kivy library
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.lang import Builder
 from kivy.app import App
 from kivy.config import Config
 Config.set('graphics', 'fullscreen', 0)
 
 
+# Connect to the database and create a database session
+engine = create_engine('sqlite:///ekt_dictionary.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
 class AddScreen(Screen):
+    def print_add_values(self):
+        print("Tagalog entry: {}".format(self.ids.t_input.text))
+        print("English entry: {}".format(self.ids.e_input.text))
+        print("Kapampangan entry: {}".format(self.ids.k_input.text))
+
+    def add_entry(self):
+        new_dict_entry = Dictionary(tagalog=self.ids.t_input.text,
+                                    kapampangan=self.ids.k_input.text,
+                                    english=self.ids.e_input.text)
+        session.add(new_dict_entry)
+        session.commit()
+
+
+class DictInput(TextInput):
+    def on_enter(self):
+        print("The value of Tagalog Text Input: {}".format(self.text))
+
+
+class MyAnc(AnchorLayout):
     pass
 
 
