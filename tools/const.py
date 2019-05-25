3b003dae944ac7ff6df88ed3f574284ed3e983f4
Matthew Yasul <yasulmatthew@gmail.com>
2019-05-25 18:59:38 +0800

    Update constants

--- tools/const.py
+++ tools/const.py
@@ -1,13 +1,10 @@
 import os
 
-SEARCH_MODES = [
-    "{}",   # Exact match
-    "{}%",  # Starts with
-    "%{}%"  #
-]
+SEARCH_MODES = ["{}", "{}%", "%{}%"]  # Exact match  # Starts with  #
 
-DICTIONARY_CSV = 'ekt_entries.csv'
-SCREENS_CSV = 'screens.csv'
-CSV_PATH = os.path.dirname(os.path.abspath(__file__)) + '/../csvs/'
+DICTIONARY_CSV = "ekt_entries.csv"
+SCREENS_CSV = "screens.csv"
+CSV_PATH = os.path.dirname(os.path.abspath(__file__)) + "/../csvs/"
 MAX_ENTRIES = 30
 ROW_DEFAULT_HEIGHT = 100
+VALID_LANGUAGES = ["kapampangan", "tagalog", "english"]
